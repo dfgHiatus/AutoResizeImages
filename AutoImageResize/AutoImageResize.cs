554fd8b1bd5529fec33a44809e9f95fb856e9fb3
dfgHiatus <evansBM@protonmail.com>
2023-10-10 08:05:35 -0700

    Your commit message here

--- AutoImageResize/AutoImageResize.cs
+++ AutoImageResize/AutoImageResize.cs
@@ -1,111 +1,99 @@
 ﻿using HarmonyLib;
-using NeosModLoader;
+using ResoniteModLoader;
 using FrooxEngine;
 using System.Threading.Tasks;
-using BaseX;
-using CodeX;
+using Elements.Core;
+using Elements.Assets;
 using System;
 using System.IO;
 
-namespace ModNameGoesHere
+namespace AutoImageResize;
+
+public class AutoImageResize : ResoniteMod
 {
-    public class AutoImageResize : NeosMod
+    [AutoRegisterConfigKey]
+    public readonly static ModConfigurationKey<int> MAX_SIZE_KEY = 
+        new("max_size", "Maximum size for Square, Power-Of-2 Textures", () => 2048);
+
+    [AutoRegisterConfigKey]
+    public readonly static ModConfigurationKey<bool> IS_ENABLED = new
+        ("is_enabled", "Enabled", () => true);
+
+    public static ModConfiguration config;
+
+    public override string Name => "AutoImageResize";
+    public override string Author => "dfgHiatus";
+    public override string Version => "2.0.0";
+    public override string Link => "https://github.com/dfgHiatus/AutoImageResize/";
+    public override void OnEngineInit()
     {
-        [AutoRegisterConfigKey]
-        public static ModConfigurationKey<int> MAX_SIZE_KEY = new ModConfigurationKey<int>("max_size", "Maximum size for Square, Power-Of-2 Textures", () => 2048);
+        config = GetConfiguration();
+        new Harmony("net.dfgHiatus.AutoImageResize").PatchAll();
+    }
 
-        [AutoRegisterConfigKey]
-        public static ModConfigurationKey<bool> IS_ENABLED = new ModConfigurationKey<bool>("is_enabled", "A toggle for the user, if the mod should run", () => true);
-
-        public static ModConfiguration config;
-
-        public override string Name => "AutoImageResize";
-        public override string Author => "dfgHiatus";
-        public override string Version => "1.0.1";
-        public override string Link => "https://github.com/dfgHiatus/AutoImageResize/";
-        public override void OnEngineInit()
+    [HarmonyPatch(typeof(ImageImporter), "ImportImage")]
+    public class AutoImageResizePatch
+    {
+        public static bool Prefix(string path, ref Task __result, Slot targetSlot, float3? forward, StereoLayout stereoLayout, ImageProjection projection, bool setupScreenshotMetadata, bool addCollider)
         {
-            config = GetConfiguration();
-            Harmony harmony = new Harmony("net.dfgHiatus.AutoImageResize");
-            harmony.PatchAll();
-        }
+            var bitmap2D = Bitmap2D.Load(path, true, Elements.Assets.AlphaHandling.KeepOriginal, int.MaxValue, 1f);
+            var imgPath = Path.Combine(Path.GetTempPath(), $"{Path.GetTempFileName()}.png");
 
-        [HarmonyPatch(typeof(ImageImporter), "ImportImage")]
-        class ModNameGoesHerePatch
-        {
-            public static bool Prefix(string path, ref Task __result, Slot targetSlot, float3? forward, StereoLayout stereoLayout, ImageProjection projection, bool setupScreenshotMetadata, bool addCollider)
+            // Also don't resize a non-square image that isn't a power of 2
+            if (bitmap2D != null &&
+                bitmap2D.Size.X > config.GetValue(MAX_SIZE_KEY) &&
+                MathX.IsPowerOfTwo(bitmap2D.Size.X) &&
+                bitmap2D.Size.X == bitmap2D.Size.Y
+                && config.GetValue(IS_ENABLED)
+                )
             {
-                var bitmap2D = Bitmap2D.Load(path, true, true, CodeX.AlphaHandling.KeepOriginal, int.MaxValue, 1f);
-                string imgPath = Path.Combine(Engine.Current.AppPath, "nml_mods", 
-                    $"{Path.GetFileNameWithoutExtension(path)}_resizedTo{config.GetValue(MAX_SIZE_KEY)}.png"); // or just use {Path.GetExtension(path)}
-
-                // Also don't resize a non-square image that isn't a power of 2
-                if (bitmap2D != null &&
-                    bitmap2D.Size.X > config.GetValue(MAX_SIZE_KEY) &&
-                    MathX.IsPowerOfTwo(bitmap2D.Size.X) &&
-                    bitmap2D.Size.X == bitmap2D.Size.Y
-                    && config.GetValue(IS_ENABLED)
-                    )
+                __result = targetSlot.StartTask(async delegate ()
                 {
-                    __result = targetSlot.StartTask(async delegate ()
+                    await default(ToBackground);
+                    bitmap2D = bitmap2D.GetRescaled(config.GetValue(MAX_SIZE_KEY), new bool?(false));
+                    bitmap2D.Save(imgPath);
+
+                    LocalDB localDB = targetSlot.World.Engine.LocalDB;
+                    Uri localUri = await localDB.ImportLocalAssetAsync(imgPath, LocalDB.ImportLocation.Copy).
+                        ConfigureAwait(continueOnCapturedContext: false);
+                    File.Delete(imgPath);
+                    await default(ToWorld);
+
+                    targetSlot.Name = Path.GetFileNameWithoutExtension(imgPath);
+                    if (forward.HasValue)
                     {
-                        await default(ToBackground);
-
-                        Debug("Saving Bitmap...");
-                        bitmap2D = bitmap2D.GetRescaled(config.GetValue(MAX_SIZE_KEY), new bool?(false));
-                        bitmap2D.Save(imgPath);
-                        Debug($"Image saved as {imgPath}");
-
-                        LocalDB localDB = targetSlot.World.Engine.LocalDB;
-                        Debug("Loading image URI from DB. URI" + imgPath);
-                        Uri localUri = await localDB.ImportLocalAssetAsync(imgPath, LocalDB.ImportLocation.Copy).ConfigureAwait(continueOnCapturedContext: false);
-                        Debug("Loading checks out.");
-
-                        File.Delete(imgPath);
-                        Debug("Deleted File");
-
-                        await default(ToWorld);
-
-                        Debug("Setting up comps...");
-                        targetSlot.Name = Path.GetFileNameWithoutExtension(imgPath);
-                        if (forward.HasValue)
+                        float3 from = forward.Value;
+                        float3 to = float3.Forward;
+                        targetSlot.LocalRotation = floatQ.FromToRotation(in from, in to);
+                    }
+                    StaticTexture2D tex = targetSlot.AttachComponent<StaticTexture2D>();
+                    tex.URL.Value = localUri;
+                    ImageImporter.SetupTextureProxyComponents(targetSlot, tex, stereoLayout, projection, setupScreenshotMetadata);
+                    if (projection != 0)
+                    {
+                        ImageImporter.Create360Sphere(targetSlot, tex, stereoLayout, projection, addCollider);
+                    }
+                    else
+                    {
+                        while (!tex.IsAssetAvailable)
                         {
-                            float3 from = forward.Value;
-                            float3 to = float3.Forward;
-                            targetSlot.LocalRotation = floatQ.FromToRotation(in from, in to);
+                            await default(NextUpdate);
                         }
-                        StaticTexture2D tex = targetSlot.AttachComponent<StaticTexture2D>();
-                        tex.URL.Value = localUri;
-                        ImageImporter.SetupTextureProxyComponents(targetSlot, tex, stereoLayout, projection, setupScreenshotMetadata);
-                        if (projection != 0)
-                        {
-                            ImageImporter.Create360Sphere(targetSlot, tex, stereoLayout, projection, addCollider);
-                        }
-                        else
-                        {
-                            while (!tex.IsAssetAvailable)
-                            {
-                                await default(NextUpdate);
-                            }
-                            ImageImporter.CreateQuad(targetSlot, tex, stereoLayout, addCollider);
-                        }
-                        if (setupScreenshotMetadata)
-                        {
-                            targetSlot.GetComponentInChildren<PhotoMetadata>()?.NotifyOfScreenshot();
-                        }
+                        ImageImporter.CreateQuad(targetSlot, tex, stereoLayout, addCollider);
+                    }
+                    if (setupScreenshotMetadata)
+                    {
+                        targetSlot.GetComponentInChildren<PhotoMetadata>()?.NotifyOfScreenshot();
+                    }
 
-                        QuadMesh _QuadMesh = targetSlot.GetComponent<QuadMesh>();
-                        _QuadMesh.Size.Value = new float2(config.GetValue(MAX_SIZE_KEY), config.GetValue(MAX_SIZE_KEY)).Normalized;
+                    QuadMesh _QuadMesh = targetSlot.GetComponent<QuadMesh>();
+                    _QuadMesh.Size.Value = new float2(config.GetValue(MAX_SIZE_KEY), config.GetValue(MAX_SIZE_KEY)).Normalized;
 
-                    });
-                    return false;
-                }
-                else
-                {
-                    Debug("Imported image is not a square power of two. Image will not be resized.");
-                    return true;
-                }
+                });
+                return false;
             }
+
+            return true;
         }
     }
 }
--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 #  AutoResizeImages
 
-A [NeosModLoader](https://github.com/zkxs/NeosModLoader) mod for [Neos VR](https://neos.com/) that automatically resizes large square power-of-2 textures upon import into NeosVR. Defaults to 2K texture resolution (2048 * 2048), can be specified in the NML config.
+A [ResoniteModLoader](https://github.com/resonite-modding-group/ResoniteModLoader) mod for [Resonite](https://resonite.com/) that automatically resizes large square power-of-2 textures upon import into Resonite. Defaults to 2K texture resolution (2048 * 2048), can be specified in the NML config.
 
 ## Installation
-1. Install [NeosModLoader](https://github.com/neos-modding-group/NeosModLoader/releases).
-1. Place [AutoResizeImages.dll](https://github.com/GithubUsername/RepoName/releases/latest/download/ModNameGoesHere.dll) into your `nml_mods` folder. This folder should be at `C:\Program Files (x86)\Steam\steamapps\common\NeosVR\nml_mods` for a default install. You can create it if it's missing, or if you launch the game once with NeosModLoader installed it will create the folder for you.
-1. Start the game. If you want to verify that the mod is working you can check your Neos logs.
+1. Install [ResoniteModLoader](https://github.com/neos-modding-group/ResoniteModLoader/releases).
+1. Place [AutoResizeImages.dll](https://github.com/GithubUsername/RepoName/releases/latest/download/ModNameGoesHere.dll) into your `rml_mods` folder. This folder should be at `C:\Program Files (x86)\Steam\steamapps\common\Resonite\rml_mods` for a default install. You can create it if it's missing, or if you launch the game once with ResoniteModLoader installed it will create the folder for you.
+1. Start the game. If you want to verify that the mod is working you can check your Resonite logs.
